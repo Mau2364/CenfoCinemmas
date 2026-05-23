f36b934241a6e84ca7673a5267b2b7dc7f5c1752
Mau2364 <jbejaranoa@ucenfotec.ac.cr>
2026-05-23 11:24:58 -0600

    Correccion SqlDao

--- DataAccess/DAO/SqlDao.cs
+++ DataAccess/DAO/SqlDao.cs
@@ -8,50 +8,57 @@ namespace DataAccess.DAO
 {
     /*
      Clase que se encarga de la comunicacion con la bd,
-    solo ejecuta store procedures
-    Implementa el patron singleton, para asegurar que solo sea un objeto el que se conecta a la bd
-    y centraliza esta gestion
-     */
+     solo ejecuta store procedures.
+
+     Implementa el patron singleton, para asegurar que solo sea un objeto el que se conecta a la bd
+     y centraliza esta gestion
+    */
     public class SqlDao
     {
-        //Paso 1: Crear una instancia privada de esta misma clase
+        // Paso 1: Crear una instancia privada de esta misma clase
         private static SqlDao instance;
 
         public CommandType CommandType { get; private set; }
 
         private string connectionString;
 
-        //paso 2: Redefinir el constructor default de la clase
-        private SqlDao () { 
-            connectionString = string .Empty;
+        // Paso 2: Redefinir el constructor default de la clase
+        private SqlDao()
+        {
+            connectionString = string.Empty;
         }
 
-        // paso 3: definir un metodo estatico que expone la instancia
-        public static SqlDao getInstance () {
+        // Paso 3: definir un metodo estatico que expone la instancia
+        public static SqlDao getInstance()
+        {
             if (instance == null)
             {
-                instance = new SqlDao ();
+                instance = new SqlDao();
             }
+
             return instance;
-    }
+        }
+
         // metodo que ejecuta de acuerdo a la especificacion del parametro
-        public void ExecuteProcedure (SqlOperation sqlOperation)
+        public void ExecuteProcedure(SqlOperation sqlOperation)
         {
-            using (var conn= new SqlConnection(connectionString))
+            using (var conn = new SqlConnection(connectionString))
             {
-                using (var command = new SqlCommand (sqlOperation.ProcedureName, conn))
+                using (var command = new SqlCommand(sqlOperation.ProcedureName, conn))
                 {
-                    CommandType = System.Data.CommandType.StoredProcedure
-                })
-                {
-                    //set de los parameters que utiliza el SP
-                    foreach (var param in sqlOperation.Parameters) {
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    // set de los parameters que utiliza el SP
+                    foreach (var param in sqlOperation.Parameters)
+                    {
                         command.Parameters.Add(param);
-                }
+                    }
+
                     // ejecuta store procedure (SP)
                     conn.Open();
-                    command.ExecuteQuery();
+                    command.ExecuteNonQuery();
+                }
             }
         }
+    }
 }
-
